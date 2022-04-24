90975231fa058c90b12de2c46bda606d1c4e203f
PedroAlbuquerque00 <pedrorodriguesalbuquerque11@gmail.com>
2022-04-24 14:09:38 -0300

    Commit

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 61
+
+console.log(`À velocidade do seu carro é ${vel}km/h!`)
+
+if (vel > 60){ // Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
